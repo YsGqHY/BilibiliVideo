0df72c9f671bdad540d9d527eef4f51e86da177c
BingZi-233 <lhby233@outlook.com>
2023-10-18 17:03:28 +0800

    update: 添加配置文件支持

---
+++ src/main/kotlin/online/bingiz/bilibili/video/internal/config/MainConfig.kt
@@ -0,0 +1,16 @@
+package online.bingiz.bilibili.video.internal.config
+
+import taboolib.module.configuration.Config
+import taboolib.module.configuration.Configuration
+
+/**
+ * Main config
+ * 主配置文件
+ *
+ * @constructor Create empty Main config
+ */
+object MainConfig {
+    @Config(value = "config.yml", autoReload = true)
+    lateinit var config: Configuration
+        private set
+}
