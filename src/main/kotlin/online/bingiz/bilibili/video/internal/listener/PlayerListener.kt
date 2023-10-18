e94a79c2f9af4166e40b7b22ef28e328a1117bfb
BingZi-233 <lhby233@outlook.com>
2023-10-18 17:04:58 +0800

    update: 添加事件监听器支持

---
+++ src/main/kotlin/online/bingiz/bilibili/video/internal/listener/PlayerListener.kt
@@ -0,0 +1,17 @@
+package online.bingiz.bilibili.video.internal.listener
+
+import org.bukkit.event.player.PlayerLoginEvent
+import org.bukkit.event.player.PlayerQuitEvent
+import taboolib.common.platform.event.SubscribeEvent
+
+object PlayerListener {
+    @SubscribeEvent
+    fun onPlayerLoginEvent(event: PlayerLoginEvent) {
+
+    }
+
+    @SubscribeEvent
+    fun onPlayerQuitEvent(event: PlayerQuitEvent) {
+
+    }
+}
