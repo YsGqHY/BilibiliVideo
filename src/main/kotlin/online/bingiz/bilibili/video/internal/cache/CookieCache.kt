71ab7a238266f933ac9d6fb29615c144299f6415
BingZi-233 <lhby233@outlook.com>
2023-10-24 11:12:48 +0800

    update: 改善缓存运行逻辑

--- src/main/kotlin/online/bingiz/bilibili/video/internal/cache/CookieCache.kt
+++ src/main/kotlin/online/bingiz/bilibili/video/internal/cache/CookieCache.kt
@@ -4,9 +4,7 @@ import com.github.benmanes.caffeine.cache.Caffeine
 import com.github.benmanes.caffeine.cache.RemovalCause
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import online.bingiz.bilibili.video.internal.util.infoMessageAsLang
-import org.bukkit.Bukkit
-import taboolib.expansion.getDataContainer
+import taboolib.expansion.getPlayerDataContainer
 import java.util.*
 
 // GSON序列化组件
@@ -19,12 +17,11 @@ val cookieCache = Caffeine.newBuilder()
     .maximumSize(100)
     .removalListener<UUID, List<String>> { key, value, cause ->
         if (cause == RemovalCause.EXPLICIT && key != null) {
-            Bukkit.getPlayer(key)?.getDataContainer()?.set("cookie", gson.toJson(value))
-                ?: infoMessageAsLang("WarnPlayerNotOnline")
+            key.getPlayerDataContainer()["cookie"] = gson.toJson(value)
         }
     }
     .build<UUID, List<String>> {
-        Bukkit.getPlayer(it)?.getDataContainer()?.get("cookie")?.let { json ->
+        it.getPlayerDataContainer()["cookie"]?.let { json ->
             gson.fromJson(json, listStringType)
         }
     }
