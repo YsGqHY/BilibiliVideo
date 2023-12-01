84fb1d40fd600174b38b7016c6677d5567eecc1a
BingZi-233 <lhby233@outlook.com>
2023-12-01 16:24:14 +0800

    update: Kehter执行器结构优化

--- src/main/kotlin/online/bingiz/bilibili/video/internal/helper/KetherHelper.kt
+++ src/main/kotlin/online/bingiz/bilibili/video/internal/helper/KetherHelper.kt
@@ -5,19 +5,36 @@ import taboolib.library.kether.LocalizedException
 import taboolib.module.kether.KetherShell.eval
 import taboolib.module.kether.ScriptOptions
 
-fun List<String>.ketherEval(sender: CommandSender) {
-    try {
-        eval(this, ScriptOptions.builder().sender(sender).build())
-    } catch (e: LocalizedException) {
-        e.message?.let { sender.warningAsLang("KetherLocalizedException", it) }
-    } catch (e: Throwable) {
-        e.message?.let { sender.warningAsLang("KetherThrowable", it) }
-    }
+/**
+ * Kether eval
+ * Kether 脚本
+ *
+ * @param sender 执行者
+ */
+fun String.ketherEval(sender: CommandSender) {
+    evalScript(listOf(this), sender)
 }
 
-fun String.ketherEval(sender: CommandSender) {
+/**
+ * Kether eval
+ * Kether 脚本
+ *
+ * @param sender 执行者
+ */
+fun List<String>.ketherEval(sender: CommandSender) {
+    evalScript(this, sender)
+}
+
+/**
+ * Eval script
+ * 脚本执行
+ *
+ * @param script 脚本
+ * @param sender 执行者
+ */
+private fun evalScript(script: List<String>, sender: CommandSender) {
     try {
-        eval(this, ScriptOptions.builder().sender(sender).build())
+        eval(script, ScriptOptions.builder().sender(sender).build())
     } catch (e: LocalizedException) {
         e.message?.let { sender.warningAsLang("KetherLocalizedException", it) }
     } catch (e: Throwable) {
