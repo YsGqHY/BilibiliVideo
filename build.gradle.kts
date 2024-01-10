58a92811a5dbab0a0412ccc72f37c6e337a23c87
BingZi-233 <lhby233@outlook.com>
2024-01-10 12:44:53 +0800

    update: TabooLib版本更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ taboolib {
     install("platform-bukkit")
     install("expansion-command-helper")
     classifier = null
-    version = "6.0.12-68"
+    version = "6.0.12-69"
 }
 
 repositories {
