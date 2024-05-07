03e131373ad9175fb419c64197b764ad05e0619f
BingZi-233 <lhby233@outlook.com>
2024-05-07 17:27:15 +0800

    fix: 约束二维码地图构建方式

--- src/main/kotlin/online/bingzi/bilibili/video/internal/helper/PlayerHelper.kt
+++ src/main/kotlin/online/bingzi/bilibili/video/internal/helper/PlayerHelper.kt
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit
 import taboolib.common.platform.ProxyPlayer
 import taboolib.module.nms.MinecraftVersion
 import taboolib.module.nms.NMSMap
+import taboolib.module.nms.buildMap
 import taboolib.module.nms.sendMap
 import taboolib.platform.util.ItemBuilder
 import java.awt.image.BufferedImage
@@ -17,8 +18,12 @@ fun ProxyPlayer.sendMapVersionCompatible(
 ) {
     Bukkit.getPlayer(this.uniqueId)?.let {
         when (MinecraftVersion.major) {
-            MinecraftVersion.V1_20 -> {
-                taboolib.module.nms.buildMap(image, hand, width, height, builder).sendTo(it)
+            in MinecraftVersion.V1_18..MinecraftVersion.V1_20 -> {
+                buildMap(image, hand, width, height, builder).sendTo(it)
+            }
+
+            in MinecraftVersion.V1_8..MinecraftVersion.V1_17 -> {
+                it.sendMap(image, hand, width, height, builder)
             }
 
             else -> {
