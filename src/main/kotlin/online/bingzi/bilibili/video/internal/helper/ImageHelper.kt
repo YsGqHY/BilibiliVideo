858cf87a197160434fb9aa5d7b6b5ee70cc06f14
BingZi-233 <lhby233@outlook.com>
2024-07-03 12:28:18 +0800

    update: 虚拟物品支持逻辑更新(未实装)

--- src/main/kotlin/online/bingzi/bilibili/video/internal/helper/ImageHelper.kt
+++ src/main/kotlin/online/bingzi/bilibili/video/internal/helper/ImageHelper.kt
@@ -1,9 +1,14 @@
 package online.bingzi.bilibili.video.internal.helper
 
+import com.comphenix.protocol.PacketType
+import com.comphenix.protocol.ProtocolLibrary
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.qrcode.QRCodeWriter
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 import java.awt.Color
 import java.awt.image.BufferedImage
+
 
 /**
  * To buffered image
@@ -29,4 +34,30 @@ fun String.toBufferedImage(size: Int): BufferedImage {
     }
     graphics.dispose()
     return image
+}
+
+/**
+ * Send virtual item
+ * 发送虚拟物品给调用玩家
+ *
+ * @param itemStack
+ * @param slot
+ */
+fun Player.sendVirtualItem(itemStack: ItemStack, slot: Int) {
+    // 创建一个装载物品数据的包
+    val packet = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.SET_SLOT)
+
+    // 设置包中的数据
+    packet.integers.write(0, 0)  // 窗口ID，0表示主背包
+    packet.integers.write(1, slot)  // 插槽ID，指定要放置物品的插槽编号
+
+    // 将物品堆栈放入包中
+    packet.itemModifier.write(0, itemStack)
+
+    try {
+        // 发送包给指定的玩家
+        ProtocolLibrary.getProtocolManager().sendServerPacket(this, packet)
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
 }
