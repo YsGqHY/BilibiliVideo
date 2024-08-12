701091c22c3ed386fb5c96eea04627181b5cf583
BingZi-233 <lhby233@outlook.com>
2024-08-12 17:13:18 +0800

    update: 移除K2编译器

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,11 +70,3 @@ configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
-
-kotlin {
-    sourceSets.all {
-        languageSettings {
-            languageVersion = "2.0"
-        }
-    }
-}
