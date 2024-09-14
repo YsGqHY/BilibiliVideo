ccdf1e80a3fe446a51dc630d08a2f5de2d39c67f
BingZi-233 <lhby233@outlook.com>
2024-09-14 15:05:05 +0800

    update: 对代码内容进行注释

--- project/core/src/main/kotlin/online/bingzi/bilibili/video/internal/config/MainConfig.kt
+++ project/core/src/main/kotlin/online/bingzi/bilibili/video/internal/config/MainConfig.kt
@@ -5,21 +5,41 @@ import taboolib.common.platform.Awake
 import taboolib.module.configuration.Config
 import taboolib.module.configuration.Configuration
 
+/**
+ * MainConfig对象用于管理主配置文件和调试状态。
+ *
+ * @author BingZi-233
+ * @since 2.0.0
+ */
 object MainConfig {
+    /**
+     * 配置文件的引用，使用config.yml文件进行配置，并支持自动重载。
+     */
     @Config(value = "config.yml", autoReload = true)
     lateinit var config: Configuration
         private set
 
+    /**
+     * 调试状态，指示当前是否处于调试模式。
+     */
     var debugStatus: Boolean = false
 
+    /**
+     * 加载配置的方法，从配置文件中读取调试状态。
+     * 在生命周期启用时调用。
+     */
     @Awake(LifeCycle.ENABLE)
     fun load() {
         debugStatus = config.getBoolean("debug")
     }
 
+    /**
+     * 注册自动重载的方法，当配置文件发生变化时重新加载配置。
+     * 在生命周期启用时调用。
+     */
     @Awake(LifeCycle.ENABLE)
     fun registerAutoReload() {
-
         config.onReload { load() }
     }
 }
+
--- project/core/src/main/kotlin/online/bingzi/bilibili/video/internal/config/VideoConfig.kt
+++ project/core/src/main/kotlin/online/bingzi/bilibili/video/internal/config/VideoConfig.kt
@@ -6,13 +6,29 @@ import taboolib.common.platform.Awake
 import taboolib.module.configuration.Config
 import taboolib.module.configuration.Configuration
 
+/**
+ * VideoConfig对象用于管理视频配置和视频数据。
+ *
+ * @author BingZi-233
+ * @since 2.0.0
+ */
 object VideoConfig {
+    /**
+     * 配置文件的引用，使用video.yml文件进行配置，并支持自动重载。
+     */
     @Config(value = "video.yml", autoReload = true)
     lateinit var config: Configuration
         private set
 
+    /**
+     * 存储视频数据的映射，键为视频标识符，值为Video对象。
+     */
     var videoData: Map<String, Video> = mapOf()
 
+    /**
+     * 加载视频数据的方法，将配置文件中的视频信息读取到videoData中。
+     * 在生命周期启用时调用。
+     */
     @Awake(LifeCycle.ENABLE)
     fun load() {
         videoData = config.getKeys(false).associateWith {
@@ -23,8 +39,33 @@ object VideoConfig {
         }
     }
 
+    /**
+     * 注册自动重载的方法，当配置文件发生变化时重新加载视频数据。
+     * 在生命周期启用时调用。
+     */
     @Awake(LifeCycle.ENABLE)
     fun registerAutoReload() {
         config.onReload { load() }
     }
+
+    /**
+     * 检查给定的视频标识符是否存在于视频数据中。
+     *
+     * @param bv 视频标识符
+     * @return 如果存在则返回true，否则返回false
+     */
+    fun checkVideo(bv: String): Boolean {
+        return videoData.containsKey(bv)
+    }
+
+    /**
+     * 根据视频标识符获取对应的Video对象。
+     *
+     * @param bv 视频标识符
+     * @return 对应的Video对象，如果不存在则返回null
+     */
+    fun getVideo(bv: String): Video? {
+        return videoData[bv]
+    }
 }
+
