39ae508d68894524de5562527558b42fd1a1e43a
BingZi-233 <lhby233@outlook.com>
2024-10-10 17:36:28 +0800

    update: 登出命令实装

--- project/runtime-bukkit/build.gradle.kts
+++ project/runtime-bukkit/build.gradle.kts
@@ -1,9 +1,13 @@
 dependencies {
     // 引入 API
-    compileOnly(project(":project:core"))
+    compileOnly(project(":project:api"))
     // 引入 服务端核心
     compileOnly("ink.ptms.core:v12004:12004:mapped")
     compileOnly("ink.ptms.core:v12004:12004:universal")
+    // ORMLite 核心
+    compileOnly("com.j256.ormlite:ormlite-core:6.1")
+    // ORMLite JDBC 驱动
+    compileOnly("com.j256.ormlite:ormlite-jdbc:6.1")
 }
 
 // 子模块
--- project/runtime-bukkit/src/main/kotlin/online/bingzi/bilibili/video/internal/command/CommandLogout.kt
+++ project/runtime-bukkit/src/main/kotlin/online/bingzi/bilibili/video/internal/command/CommandLogout.kt
@@ -1,9 +1,39 @@
 package online.bingzi.bilibili.video.internal.command
 
+import online.bingzi.bilibili.video.api.BilibiliVideoAPI
+import taboolib.common.platform.ProxyCommandSender
+import taboolib.common.platform.ProxyPlayer
 import taboolib.common.platform.command.subCommand
+import taboolib.common.platform.command.suggestPlayers
+import taboolib.common.platform.function.getProxyPlayer
+import taboolib.module.lang.sendInfo
+import taboolib.module.lang.sendWarn
 
+/**
+ * Command logout
+ * <p>
+ * 命令·登出
+ *
+ * @constructor Create empty Command logout
+ *
+ * @author BingZi-233
+ * @since 2.0.0
+ */
 object CommandLogout {
     var execute = subCommand {
-
+        execute<ProxyPlayer> { sender, _, _ ->
+            BilibiliVideoAPI.getPlayerCookieEntity(sender.uniqueId)?.delete()
+            sender.sendInfo("commandLogoutSuccess")
+        }
+        dynamic(comment = "player", optional = true, permission = "BilibiliVideo.command.logout.player") {
+            suggestPlayers()
+            execute<ProxyCommandSender> { sender, context, _ ->
+                val playerName = context["player"]
+                getProxyPlayer(playerName)?.let {
+                    BilibiliVideoAPI.getPlayerCookieEntity(it.uniqueId)?.delete()
+                    sender.sendInfo("commandLogoutPlayerSuccess", playerName)
+                } ?: sender.sendWarn("playerNotFound", playerName)
+            }
+        }
     }
 }
