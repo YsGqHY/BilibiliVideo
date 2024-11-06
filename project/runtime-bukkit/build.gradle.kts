f8def2204f5b915357ecd3ae0b7354bb6921352e
BingZi-233 <lhby233@outlook.com>
2024-11-06 23:04:18 +0800

    update: 调整依赖顺序

--- project/runtime-bukkit/build.gradle.kts
+++ project/runtime-bukkit/build.gradle.kts
@@ -5,13 +5,13 @@ dependencies {
     compileOnly(project(":project:core"))
     // 引入 NMS 模块
     compileOnly(project(":project:nms"))
-    // 引入 服务端 核心
-    compileOnly("ink.ptms.core:v12004:12004:mapped")
-    compileOnly("ink.ptms.core:v12004:12004:universal")
     // ORMLite 核心
     compileOnly("com.j256.ormlite:ormlite-core:6.1")
     // ORMLite JDBC 驱动
     compileOnly("com.j256.ormlite:ormlite-jdbc:6.1")
+    // 引入 服务端 核心
+    compileOnly("ink.ptms.core:v12004:12004:mapped")
+    compileOnly("ink.ptms.core:v12004:12004:universal")
 }
 
 // 子模块
