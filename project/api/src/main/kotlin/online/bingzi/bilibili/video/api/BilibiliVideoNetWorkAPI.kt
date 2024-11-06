8f43abd17044551fa1e0993205a147b2b5ac81cf
BingZi-233 <lhby233@outlook.com>
2024-11-06 23:47:41 +0800

    fix: 修复无法正常完成登录流程

--- project/api/src/main/kotlin/online/bingzi/bilibili/video/api/BilibiliVideoNetWorkAPI.kt
+++ project/api/src/main/kotlin/online/bingzi/bilibili/video/api/BilibiliVideoNetWorkAPI.kt
@@ -1,22 +1,21 @@
 package online.bingzi.bilibili.video.api
 
-import kotlinx.coroutines.delay
 import online.bingzi.bilibili.video.api.event.BilibiliQRCodeWriteCacheEvent
 import online.bingzi.bilibili.video.internal.cache.Cache
 import online.bingzi.bilibili.video.internal.config.MainConfig
+import online.bingzi.bilibili.video.internal.entity.GenerateData
 import online.bingzi.bilibili.video.internal.entity.ReleasesData
+import online.bingzi.bilibili.video.internal.entity.ResultVO
 import online.bingzi.bilibili.video.internal.helper.ImageHelper
+import online.bingzi.bilibili.video.internal.helper.debug
 import online.bingzi.bilibili.video.internal.network.Network
 import org.bukkit.entity.Player
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.await
 import taboolib.common.platform.ProxyCommandSender
 import taboolib.common.platform.ProxyPlayer
 import taboolib.common.platform.function.pluginVersion
-import taboolib.expansion.DurationType
-import taboolib.expansion.chain
 import taboolib.module.lang.sendInfo
 import taboolib.module.lang.sendWarn
 import taboolib.platform.util.bukkitPlugin
@@ -40,32 +39,42 @@ object BilibiliVideoNetWorkAPI {
     }
 
     fun requestBilibiliGetQRCodeKey(proxyPlayer: ProxyPlayer) {
-        chain {
-            val resultVo = Network.loginService.generate().await()
-            if (resultVo.isSuccess()) {
-                Cache.qrCodeCache.put(proxyPlayer.uniqueId, resultVo.data.qrcodeKey)
-                BilibiliQRCodeWriteCacheEvent(proxyPlayer, resultVo.data.qrcodeKey).call()
-                val stringToBufferImage = ImageHelper.stringToBufferImage(resultVo.data.url)
-                if (MainConfig.settingAsyncSendPacket) {
-                    BilibiliVideoNMSAPI.sendVirtualMapToPlayerAsync(proxyPlayer.castSafely<Player>()!!, stringToBufferImage, MainConfig.settingHand)
-                } else {
-                    BilibiliVideoNMSAPI.sendVirtualMapToPlayer(proxyPlayer.castSafely<Player>()!!, stringToBufferImage, MainConfig.settingHand)
+        Network.loginService.generate().enqueue(object : Callback<ResultVO<GenerateData>> {
+            override fun onResponse(call: Call<ResultVO<GenerateData>>, response: Response<ResultVO<GenerateData>>) {
+                response.body()?.let { resultVo ->
+                    if (resultVo.isSuccess()) {
+                        Cache.qrCodeCache.put(proxyPlayer.uniqueId, resultVo.data.qrcodeKey)
+                        BilibiliQRCodeWriteCacheEvent(proxyPlayer, resultVo.data.qrcodeKey).call()
+                        val stringToBufferImage = ImageHelper.stringToBufferImage(resultVo.data.url)
+                        if (MainConfig.settingAsyncSendPacket) {
+                            BilibiliVideoNMSAPI.sendVirtualMapToPlayerAsync(proxyPlayer.castSafely<Player>()!!, stringToBufferImage, MainConfig.settingHand)
+                        } else {
+                            BilibiliVideoNMSAPI.sendVirtualMapToPlayer(proxyPlayer.castSafely<Player>()!!, stringToBufferImage, MainConfig.settingHand)
+                        }
+                    } else {
+                        proxyPlayer.sendWarn("responseFailed", resultVo.message)
+                    }
                 }
-            } else {
-                proxyPlayer.sendWarn("responseFailed", resultVo.message)
             }
-        }
+
+            override fun onFailure(call: Call<ResultVO<GenerateData>>, t: Throwable) {
+                proxyPlayer.sendWarn("networkError", t.message ?: "")
+            }
+
+        })
     }
 
-    fun requestBilibiliGetCookie(qrCodeKey: String) {
-        chain {
-            while (true) {
-                val resultVO = Network.loginService.poll(qrCodeKey).await()
+    fun requestBilibiliGetCookie(qrCodeKey: String): Boolean {
+        val voResponse = Network.loginService.poll(qrCodeKey).execute()
+        if (voResponse.isSuccessful) {
+            voResponse.body()?.let { resultVO ->
                 if (resultVO.isSuccess() && (resultVO.data.isLogin() || resultVO.data.isInvalid())) {
-                    return@chain
+                    return true
                 }
-                wait(20L, type = DurationType.MINECRAFT_TICK)
             }
+        } else {
+            debug("网络故障，错误码 ${voResponse.code()} 错误信息 ${voResponse.message()}")
         }
+        return false
     }
 }
--- project/runtime-bukkit/src/main/kotlin/online/bingzi/bilibili/video/internal/listener/BilibiliListener.kt
+++ project/runtime-bukkit/src/main/kotlin/online/bingzi/bilibili/video/internal/listener/BilibiliListener.kt
@@ -3,10 +3,16 @@ package online.bingzi.bilibili.video.internal.listener
 import online.bingzi.bilibili.video.api.BilibiliVideoNetWorkAPI
 import online.bingzi.bilibili.video.api.event.BilibiliQRCodeWriteCacheEvent
 import taboolib.common.platform.event.SubscribeEvent
+import taboolib.common.platform.function.submitAsync
 
 object BilibiliListener {
     @SubscribeEvent
     fun onBilibiliQRCodeWriteCacheEvent(event: BilibiliQRCodeWriteCacheEvent) {
-        BilibiliVideoNetWorkAPI.requestBilibiliGetCookie(event.qrCodeKey)
+        submitAsync(delay = 20L, period = 20L) {
+            val isCancel = BilibiliVideoNetWorkAPI.requestBilibiliGetCookie(event.qrCodeKey)
+            if (isCancel) {
+                this.cancel()
+            }
+        }
     }
 }
