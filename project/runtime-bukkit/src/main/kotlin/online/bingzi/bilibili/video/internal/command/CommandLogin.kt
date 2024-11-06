1f56f48fb5192e4834d72ebd8353e5452e1f3eb2
BingZi-233 <lhby233@outlook.com>
2024-11-06 22:44:53 +0800

    update: 登录命令实装

--- project/runtime-bukkit/src/main/kotlin/online/bingzi/bilibili/video/internal/command/CommandLogin.kt
+++ project/runtime-bukkit/src/main/kotlin/online/bingzi/bilibili/video/internal/command/CommandLogin.kt
@@ -1,8 +1,30 @@
 package online.bingzi.bilibili.video.internal.command
 
+import online.bingzi.bilibili.video.api.BilibiliVideoNetWorkAPI
+import taboolib.common.platform.ProxyCommandSender
+import taboolib.common.platform.ProxyPlayer
 import taboolib.common.platform.command.subCommand
+import taboolib.common.platform.command.suggestPlayers
+import taboolib.common.platform.function.getProxyPlayer
+import taboolib.module.lang.sendWarn
 
 object CommandLogin {
     val execute = subCommand {
+        execute<ProxyPlayer> { sender, _, _ ->
+            BilibiliVideoNetWorkAPI.requestBilibiliGetQRCodeKey(sender)
+        }
+        dynamic(comment = "player") {
+            suggestPlayers()
+            execute<ProxyCommandSender> { sender, context, _ ->
+                // 从上下文中获取玩家名称
+                val playerName = context["player"]
+                // 根据玩家名称获取ProxyPlayer对象，如果未找到则发送警告信息
+                val proxyPlayer = getProxyPlayer(playerName) ?: let {
+                    sender.sendWarn("playerNotFound", playerName)
+                    return@execute
+                }
+                BilibiliVideoNetWorkAPI.requestBilibiliGetQRCodeKey(proxyPlayer)
+            }
+        }
     }
 }
